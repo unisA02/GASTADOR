2f50f7fb9bdfa53e325af6c63ed490f9c894d292
unisA02 <quinxyraheunicealmeyda@gmail.com>
2025-12-27 21:10:29 +0800

    tailwindcss update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./login/*.html",      // all login HTML files
-    "./register/*.html",   // register HTML files
-    "./*.html",            // any HTML at root
-    "./src/**/*.css",      // your input.css
-    "./**/*.js"            // all JS files
+    "./login/**/*.html",      // all HTML files in login
+    "./register/**/*.html",   // all HTML files in register
+    "./*.html",               // HTML files in root
+    "./src/**/*.js"           // JS files in src
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#f97316',   // orange from your style
-        secondary: '#f87171', // red from your style
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
